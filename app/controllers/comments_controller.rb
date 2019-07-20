42dd0a4e6a2cf0e8e4b9b38e7fc577fe5b505ffb
rzeylik <rzeylik@gmail.com>
2019-07-20 11:39:49 +0300

    add authenticate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,6 @@
 class CommentsController < ApplicationController
+  # http_basic_authenticate_with name: 'admin', password: "1q2" 
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  http_basic_authenticate_with name: 'admin', password: "1q2",
+    except: [:index, :show] 
+
   def index
     @posts = Post.all
   end
